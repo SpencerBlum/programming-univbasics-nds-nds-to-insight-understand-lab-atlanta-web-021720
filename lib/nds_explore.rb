ac334b86abf60d5cebd7fe0940ca24dcf1a72138
Spencer Blum <spencerblum@gmail.com>
2020-01-29 22:39:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,9 +5,19 @@ require 'pp'
 
 def pretty_print_nds(nds)
 pp nds
-pp nds
 end
 
 def print_first_directors_movie_titles
+
+  ss_movies = directors_database[0][:movies]
+    index = 0
+
+    while index < ss_movies.length do
+      titles = ss_movies[index][:title]
+      puts titles
+      index +=1
+    end
+
+
 
 end
